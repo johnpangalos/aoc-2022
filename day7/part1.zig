e88e74cf5b91fb2e5e319fb2ce239c089a5872cf
John Pangalos <john@pangalos.dev>
2022-12-21 22:35:15 +0100

    day 7 part 1

--- day7/part1.zig
+++ day7/part1.zig
@@ -1,6 +1,16 @@
 const std = @import("std");
 const input = @embedFile("./input.txt");
 
+fn getPath(stack: std.ArrayList([]const u8)) ![]const u8 {
+    var path = std.ArrayList(u8).init(std.heap.page_allocator);
+
+    for (stack.items) |item| {
+        try path.appendSlice(item);
+        if (!std.mem.eql(u8, item, "/")) try path.append('/');
+    }
+    return path.items;
+}
+
 pub fn main() !void {
     var iter = std.mem.tokenize(u8, input, "\n");
     var map = std.StringHashMap(usize).init(
@@ -18,20 +28,23 @@ pub fn main() !void {
             continue;
         }
         if (std.mem.eql(u8, line[0..4], "$ cd")) {
-            _ = try map.getOrPutValue(line[5..], 0);
             try stack.append(line[5..]);
+            _ = try map.getOrPutValue(try getPath(stack), 0);
             continue;
         }
         if (std.mem.eql(u8, line[0..3], "dir")) {
-            _ = try map.getOrPutValue(line[4..], 0);
+            // _ = try map.getOrPutValue(getPath(stack), 0);
             continue;
         }
         var temp = std.mem.tokenize(u8, line, " ");
         var size = try std.fmt.parseInt(usize, temp.next().?, 10);
 
-        for (stack.items) |dir| {
-            var currentSize = map.get(dir) orelse 0;
-            try map.put(dir, currentSize + size);
+        for (stack.items) |_, idx| {
+            var stackClone = try stack.clone();
+            stackClone.shrinkRetainingCapacity(idx + 1);
+            var path = try getPath(stackClone);
+            var currentSize = map.get(path) orelse 0;
+            try map.put(path, currentSize + size);
         }
     }
 
@@ -39,7 +52,7 @@ pub fn main() !void {
     var total: usize = 0;
     while (mapIter.next()) |node| {
         if (node.value_ptr.* <= 100000) total += node.value_ptr.*;
-        std.debug.print("{s}, {d}\n", .{ node.key_ptr.*, node.value_ptr.* });
+        // std.debug.print("{s}, {d}\n", .{ node.key_ptr.*, node.value_ptr.* });
     }
     std.debug.print("{d}\n", .{total});
 }
