1631e43cad7cfb5d51f2d1ac913370905d9c012d
John Pangalos <john@pangalos.dev>
2022-12-21 22:51:44 +0100

    finishing part 7

--- day7/part2.zig
+++ day7/part2.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const input = @embedFile("./input.txt");
 
+const fsSize: usize = 70000000;
+const updateSize: usize = 30000000;
+
 fn getPath(stack: std.ArrayList([]const u8)) ![]const u8 {
     var path = std.ArrayList(u8).init(std.heap.page_allocator);
 
@@ -23,21 +26,22 @@ pub fn main() !void {
 
     while (iter.next()) |line| {
         if (std.mem.eql(u8, line, "$ ls")) continue;
+
         if (std.mem.eql(u8, line, "$ cd ..")) {
             _ = stack.pop();
             continue;
         }
+
         if (std.mem.eql(u8, line[0..4], "$ cd")) {
             try stack.append(line[5..]);
             _ = try map.getOrPutValue(try getPath(stack), 0);
             continue;
         }
-        if (std.mem.eql(u8, line[0..3], "dir")) {
-            continue;
-        }
+
+        if (std.mem.eql(u8, line[0..3], "dir")) continue;
+
         var temp = std.mem.tokenize(u8, line, " ");
         var size = try std.fmt.parseInt(usize, temp.next().?, 10);
-
         for (stack.items) |_, idx| {
             var stackClone = try stack.clone();
             stackClone.shrinkRetainingCapacity(idx + 1);
@@ -48,9 +52,13 @@ pub fn main() !void {
     }
 
     var mapIter = map.iterator();
-    var total: usize = 0;
+    var diskSize = map.get("/").?;
+    var min: usize = diskSize;
+
+    var spaceRequired: usize = updateSize - (fsSize - diskSize);
+
     while (mapIter.next()) |node| {
-        if (node.value_ptr.* <= 100000) total += node.value_ptr.*;
+        if (node.value_ptr.* >= spaceRequired) min = std.math.min(min, node.value_ptr.*);
     }
-    std.debug.print("{d}\n", .{total});
+    std.debug.print("{d}\n", .{min});
 }
